4e11047e16a97e00a5c1f27befe88ad3c963c55c
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-09-26 06:01:58 +0700

    Install CI3 & Latihan Controller

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Khaerul Falah
+            Saya tinggal di daerah Ciampea
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
