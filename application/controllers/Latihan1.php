a647e05e2ed0f8efeddf2555fc481d368821fa8a
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-09-30 08:30:23 +0700

    Tugas P2 mengerjakan contoh2(controllers dan model)

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,5 +4,13 @@ class Latihan1 extends CI_Controller
     public function index()
     {
         echo "Selamat Datang.. selamat belajar web programming";
+		// $this->load->view('view-latihan1');
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari " . $n1 . " + " . $n2 . " = " . $hasil;
     }
 }
