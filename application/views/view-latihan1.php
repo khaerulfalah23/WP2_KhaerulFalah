6f542bb6b008f298c2ac8e62a158736e3e8cfd86
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-09-30 08:38:41 +0700

    Tugas P2 mengerjakan contoh3(controller,model,view)

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+</head>
+<body>
+Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
