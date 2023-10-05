1296db196d43651526d7e24130f38e80ebad0363
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-10-05 18:23:43 +0700

    P3 latihan parsing data (view,controller,model) & menambahkan .htaccess

---
+++ application/views/view-data-matakuliah.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampil Data Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata kuliah
+                </th>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <hr>
+                </td>
+            </tr>
+            <tr>
+                <th>Kode MTK</th>
+                <th>:</th>
+                <td>
+                    <?= $kode; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>Nama MTK</th>
+                <th>:</th>
+                <td>
+                    <?= $nama; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>SKS</th>
+                <th>:</th>
+                <td>
+                    <?= $sks; ?>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center">
+                    <a href="<?= base_url('matakuliah'); ?>">Kembali</a>
+                </td>
+            </tr>
+        </table>
+    </center>
+</body>
+
+</html>
