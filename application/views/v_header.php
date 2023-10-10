e42da8740954a1f619bfa183771f94035f5896a4
khaerulfalah23 <khaerulfalah23@gmail.com>
2023-10-10 08:30:33 +0700

    Tugas P3 membuat template dinamis & setting base_url()

---
+++ application/views/v_header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Prog II | Merancang Template sederhana dengan codeigniter</title>
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan codeigniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?= base_url(); ?>web">Home</a></li>
+                    <li><a href="<?= base_url(); ?>web/about">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
